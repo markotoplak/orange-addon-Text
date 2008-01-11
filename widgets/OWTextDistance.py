b58cfd90b66e157060b282f1e976c971c7e2792a
blaz <blaz@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-01-11 03:30:54 +0000

    info box added

--- widgets/OWTextDistance.py
+++ widgets/OWTextDistance.py
@@ -16,20 +16,35 @@ class OWTextDistance(OWWidget):
         self.outputs = [("Distance Matrix", orange.SymMatrix)]
         self.data = None
         self.distanceType = 0
+        self.nDocuments = "N/A"; self.nWords = "N/A"
         self.loadSettings()
+
+        box = OWGUI.widgetBox(self.controlArea, "Info", addSpace = True)
+        OWGUI.label(box, self, "Documents: %(nDocuments)s")
+        OWGUI.label(box, self, "Words: %(nWords)s")
+
         OWGUI.radioButtonsInBox(self.controlArea, self, "distanceType", box = "Matrix type",
             btnLabels = ["Similarity [cos(fi)]", "Distance [1/cos(fi)]"], addSpace = True)
         OWGUI.button(self.controlArea, self, "Apply", self.apply)
         self.adjustSize()
 
     def dataset(self, data):
+        if data:
+            words = len(data.domain.getmetas(orngText.TEXTMETAID))
+            if words == 0:
+                self.error("Bag-of-words data set empty (no words)")
+                self.send("Distance Matrix", None)
+                self.nDocuments = "N/A"; self.nWords = "N/A"
+                self.data = None
+                return
+            self.nDocuments = len(data); self.nWords = words
         self.data = data
         self.apply()
 
     def apply(self):
         if self.data:
-            #pb = OWGUI.ProgressBar(self, iterations=(len(self.data) ** 2)/2.)
-            #dist = orngText.cos(self.data, distance = self.distanceType, callback = pb.advance)
+            # pb = OWGUI.ProgressBar(self, iterations=(len(self.data) ** 2)/2.)
+            # dist = orngText.cos(self.data, distance = self.distanceType, callback = pb.advance)
             pb = OWGUI.ProgressBar(self, iterations=2)
             pb.advance()
             dist = orange.textCos(self.data, self.distanceType, orngText.TEXTMETAID)
