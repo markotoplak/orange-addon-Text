8aa38ab4575da79c38918422c62cdb1bc03f54ff
tomaz <tomaz@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-10-22 07:59:13 +0000

    correct data dir name is datasets

--- textConfiguration.py
+++ textConfiguration.py
@@ -1,3 +1,3 @@
 # this file is used to store the path to preinstalled data, which is platform and installation dependent
 import os
-datasetsPath = os.path.join(os.path.split(os.path.abspath(__file__))[0], "doc", "data")
+datasetsPath = os.path.join(os.path.split(os.path.abspath(__file__))[0], "doc", "datasets")
